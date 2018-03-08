8da191a0ec2bb6b6afa1d7bd190fd52b20ea959c
Jiyong Park <jiyong@google.com>
2018-03-08 17:30:27 +0900

    asphalt: folio-daemon: Disable modules that can't be built in PDK
    Modules that can't be built in PDK are disabled when TARGET_BUILD_PDK is
    true
    
    Bug: 71361051
    Test: lunch aosp_walleye-userdebug
    PDK_FUSION_PLATFORM_ZIP=vendor/pdk/walleye/walleye-userdebug/platform/platform.zip m -j
    
    Change-Id: I2cc7ab6d667daab7d3566baf8a9b473cafc4e452

--- folio-daemon/Android.mk
+++ folio-daemon/Android.mk
@@ -1,4 +1,8 @@
 LOCAL_PATH := $(call my-dir)
+
+# Disabling when building with PDK (b/68767391)
+ifneq ($(TARGET_BUILD_PDK),true)
+
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := \
@@ -21,3 +25,5 @@ LOCAL_MODULE_OWNER := google
 LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_EXECUTABLE)
+
+endif
