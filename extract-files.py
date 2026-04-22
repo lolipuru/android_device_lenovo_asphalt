0bd04902006edbb09e5788175100f170807e7e02
lolipuru <banstellazz@gmail.com>
2026-04-22 10:09:49 +0000

    asphalt: sunset dolby
    they're 2 issues with stock dolby
    
    - first, stock asphalt dolby is 32bit blobs
    - second, stock dolby only works with speaker for some reasons
    
    so better drop it, and use sony or moto dolby as it seem to be more sane, and better choice
    
    Change-Id: Ia60f214cbc6b7b803ee37bf7ead2c16c43c97337
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -53,22 +53,6 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.vibrator-V2-ndk_platform.so',
             'android.hardware.vibrator-V2-ndk.so',
         ),
-    (
-        'vendor/bin/hw/dolbycodec2',
-        'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service',
-        'vendor/bin/hw/vendor.dolby.media.c2-default-service-dax',
-        'vendor/lib64/libdlbdsservice.so',
-        'vendor/lib64/libdlbpreg.so',
-        'vendor/lib64/soundfx/libdlbvol.so',
-        'vendor/lib64/soundfx/libswdap.so',
-        'vendor/lib64/soundfx/libswgamedap.so',
-    ): blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
-    (
-        'vendor/lib/c2.dolby.client.so',
-        'vendor/lib64/c2.dolby.client.so',
-    ): blob_fixup()
-        .add_needed('dolbycodec_shim.so'),
 }
 
 module = ExtractUtilsModule(
